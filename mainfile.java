6aa57912c08de9570adbf34d2f1b55511c73a0a3
jaditwo <111250777+jaditwo@users.noreply.github.com>
2025-04-19 14:07:23 -0500

    Update mainfile.java

--- mainfile.java
+++ mainfile.java
@@ -2,6 +2,6 @@ public class mainfile {
   public static void main(String[] args) {
     int num1 = 53;
     int num2 = 3;
-    System.out.println(num1 + num2);
+    System.out.println(num1 * num2);
   }
 }  
